a1e261295d1066b5f34e1f4351658242383faa46
NarenNades <344941885@tdsb.ca>
2026-02-12 16:29:32 +0000

    Done!

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,10 @@
+/**
+* File Name: Lesson 1.3 - Strings
+* Author: Naren Nades
+* Date Created: Feb 8, 2023
+* Date Last Modified: Feb 12, 2026
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -9,18 +16,37 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n\n* * * * *\n*       *\n*       *\n*       *\n* * * * *\n\n* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		String cat = ("          '__'\n" + //
+						"          (oo)\n" + //
+						"  /========//\n" + //
+						" / || XX ||\n" + //
+						"*  ||----||\n" + //
+						"   VV    VV\n" + //
+						"   ''    ''");
+		System.out.println(cat);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		String phraseA1 = ("Fortnite ");
+		String phraseA2 = ("is awful");
+
+		System.out.print(phraseA1);
+		System.out.println(phraseA2);
+
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		String phraseB1 = ("Don't ");
+		String phraseB2 = ("@ me");
+
+		System.out.println(phraseB1 + phraseB2);
 	}
 
 
